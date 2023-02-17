b5f9ba8094aa99e7dce174edb9bbcb02c49018b9
CarterBloop <carter@tiogaventures.com>
2023-02-16 22:18:48 -0800

    Config: Emmission

--- src/config/CryptoNoteConfig.h
+++ src/config/CryptoNoteConfig.h
@@ -72,7 +72,7 @@ namespace CryptoNote
 
         const uint64_t LWMA_2_DIFFICULTY_BLOCK_INDEX_V3 = 3;
 
-        const unsigned EMISSION_SPEED_FACTOR = 30;
+        const unsigned EMISSION_SPEED_FACTOR = 10;
 
         static_assert(EMISSION_SPEED_FACTOR <= 8 * sizeof(uint64_t), "Bad EMISSION_SPEED_FACTOR");
 
